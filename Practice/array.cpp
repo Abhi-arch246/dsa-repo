3dc6e0c9755472455c2ad4b34d6dd65fe5fd8df0
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-07-18 22:00:25 +0530

    Array program

---
+++ Practice/array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+/* Arrays intro:
+ int a[100],
+ int a[100]={0},
+ int a[100]={1,2,3},
+ int a[] = {1,2,3}
+ string fruits[4]={'abc','def','ghi'}
+*/
+
+void printArray(int arr[], int size)
+{
+    cout << "Inside print function" << endl;
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << ", ";
+}
+
+int main()
+{
+    int arr[100] = {0};
+    int size;
+    cout << "Enter the size of array" << endl;
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << ", ";
+    cout << endl;
+    printArray(arr, size);
+
+    return 0;
+}
