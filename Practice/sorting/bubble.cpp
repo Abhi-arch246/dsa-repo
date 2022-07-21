5ce261f34ccec51b6f77336a175cc7d829ccd73c
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-07-21 23:30:34 +0530

    bubble sort algo

---
+++ Practice/sorting/bubble.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int arr[], int size)
+{
+    int count = 1;
+    while (count < size)
+    {
+        for (int i = 0; i < size - count; i++)
+        {
+            if (arr[i] > arr[i + 1])
+                swap(arr[i], arr[i + 1]);
+        }
+
+        count++;
+    }
+}
+
+int main()
+{
+    int size;
+    cout << "Enter size of the array" << endl;
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter elements into the array" << endl;
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+
+    bubble_sort(arr, size);
+
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
