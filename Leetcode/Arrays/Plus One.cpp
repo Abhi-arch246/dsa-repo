6baffd036bde6ae67eb325eb248a09f290179f86
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-10-16 01:01:11 +0530

    Plus One

---
+++ Leetcode/Arrays/Plus One.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+
+        int i = digits.size() - 1;
+        while (i >= 0)
+        {
+            if (digits[i] == 9)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                digits[i] += 1;
+                return digits;
+            }
+            i--;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
